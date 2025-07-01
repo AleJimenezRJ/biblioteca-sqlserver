04096e1521dfc0ebc1da213b6d15b010f38f7675
Alejandro Jiménez Rojas <alejimenezrj@gmail.com>
2025-06-30 22:48:32 -0600

    feat: add index

--- src/1_estructura.sql
+++ src/1_estructura.sql
@@ -9,7 +9,7 @@ Este archivo contiene el codigo para la creacion de la base de datos (esquemas, 
 
 */
 
--- Crear base de datos (es necesario o puedo asumir que ya?)
+-- Crear la base de datos
 CREATE DATABASE Biblioteca;
 GO
 
@@ -26,11 +26,13 @@ GO
 CREATE SCHEMA lib; -- Para los libros
 GO
 
+-- Tablas
+
 -- Crear tabla de Usuarios en esquema usr con todos los atributos de los usuarios
 CREATE TABLE usr.Usuario (
     LlaveInterna INT NOT NULL PRIMARY KEY CLUSTERED IDENTITY(1,1),
-    Correo NVARCHAR(255) NOT NULL UNIQUE, -- El correo debe ser unico
-    Nombre NVARCHAR(100) NOT NULL,
+    Correo NVARCHAR(100) NOT NULL UNIQUE, -- El correo debe ser unico
+    Nombre NVARCHAR(100) NOT NULL, -- Se trabaja nombres en general con un NVARCHAR(100) para dar un tamaño relativamente generoso
     Apellido NVARCHAR(100) NOT NULL
 );
 GO
@@ -38,20 +40,20 @@ GO
 -- Crear tabla de Libros en esquema lib
 CREATE TABLE lib.Libro (
     LlaveInterna INT NOT NULL PRIMARY KEY CLUSTERED IDENTITY(1,1),
-    ISBN NVARCHAR(20) NOT NULL UNIQUE,
-    Titulo NVARCHAR(255) NOT NULL,
-    Autor NVARCHAR(255) NOT NULL,
-    Editorial NVARCHAR(255) NOT NULL,
+    ISBN NVARCHAR(20) NOT NULL UNIQUE, -- Valor único, se usan NVARCHAR(20) debido a que suele ser un valor entre unos 10 y 17 chars
+    Titulo NVARCHAR(100) NOT NULL,
+    Autor NVARCHAR(100) NOT NULL,
+    Editorial NVARCHAR(100) NOT NULL,
     Anno INT NOT NULL,
     Estado BIT NOT NULL DEFAULT 0, -- Booleano para saber si esta disponible o no. 0 = Disponible, 1 = Prestado
-    Tematica NVARCHAR(255) NULL -- Esto se esperaraba que fuera un texto separado por comas
+    Tematica NVARCHAR(255) NULL -- Se da un valor bastante grande por si se quisiera meter un listado de temáticas separadas por comas
 );
 GO
 
--- Crear tabla de Prestamos
+-- Crear tabla para el registro de los prestamos
 CREATE TABLE Prestamo (
     LlaveInterna INT PRIMARY KEY IDENTITY(1,1),
-    CorreoUsuario NVARCHAR(255) NOT NULL,
+    CorreoUsuario NVARCHAR(100) NOT NULL, -- Se relaciona al ISBN y correo por ser los datos únicos
     ISBNLibro NVARCHAR(20) NOT NULL,
     FechaPrestamo DATE NOT NULL,
     FechaDevolucionPrevista DATE NOT NULL,
@@ -61,14 +63,31 @@ CREATE TABLE Prestamo (
 );
 GO
 
+-- Crear la tabla para registrar la bitacora de los prestamos y devoluciones. No se usa FK por persistencia de datos.
 CREATE TABLE BitacoraPrestamos (
-    LlaveInterna INT PRIMARY KEY IDENTITY(1,1),
-    CorreoUsuario NVARCHAR(255) NOT NULL,
+    LlaveInterna INT PRIMARY KEY IDENTITY(1,1), -- Igual que todos, usa llave interna
+    CorreoUsuario NVARCHAR(100) NOT NULL,
     ISBNLibro NVARCHAR(20) NOT NULL,
     FechaOperacion DATETIME NOT NULL DEFAULT GETDATE(),
-    TipoOperacion NVARCHAR(20) NOT NULL CHECK (TipoOperacion IN ('Prestamo', 'Devolucion'))
+    TipoOperacion NVARCHAR(20) NOT NULL CHECK (TipoOperacion IN ('Prestamo', 'Devolucion')) -- 2 posibles actions
 );
 GO
 
 
--- INDICES PENDIENTES
+-- Indices para intentar optimizar lo más que se pueda las consultas.
+
+-- Para optimizar una consulta a la bitacora cuando se buscan los datos de un usuario especifico
+CREATE NONCLUSTERED INDEX IX_Bitacora_Correo ON BitacoraPrestamos (CorreoUsuario, FechaOperacion);
+
+-- Para la consulta de los 5 libros mas prestados
+CREATE NONCLUSTERED INDEX IX_Prestamo ON Prestamo (ISBNLibro);
+
+-- Para mejorar joins de tables
+CREATE UNIQUE NONCLUSTERED INDEX IX_Libro_ISBN ON lib.Libro (ISBN);
+
+-- Para mejorar joins de tables
+CREATE UNIQUE NONCLUSTERED INDEX IX_Usuario_Correo ON usr.Usuario (Correo);
+
+-- Para optimizar la consulta de los prestamos activos
+CREATE NONCLUSTERED INDEX IX_Prestamos_Activos ON Prestamo (CorreoUsuario, FechaDevolucionReal)
+WHERE FechaDevolucionReal IS NULL;
